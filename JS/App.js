2544dd30e5bdc095d72671981444f57c010055c3
Oluwamuyiwa_Ale <141297202+Oluwamuyiwa01@users.noreply.github.com>
2024-01-20 03:31:39 -0800

    Add files via upload

---
+++ JS/App.js
@@ -0,0 +1,18 @@
+const textareaEl = document.getElementById('textarea');
+
+const totalCounterEl = document.getElementById('total-counter');
+
+const remainingCounterEl = document.getElementById('remaining-counter');
+
+textareaEl.addEventListener('keyup', ()=>{
+    updateCounter();
+})
+
+updateCounter();
+
+function updateCounter(){
+  totalCounterEl.innerHTML = textareaEl.value.length;
+
+  remainingCounterEl.innerHTML = textareaEl.getAttribute('maxLength') - totalCounterEl.innerHTML;
+}
+
